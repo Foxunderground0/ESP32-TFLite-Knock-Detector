e53e7e07e3fa98bda460da4a9c31cb3b1d06039a
Umer Irfan <54197021+Foxunderground0@users.noreply.github.com>
2023-05-10 09:42:43 +0500

    Final Arduino code to send and receive data

--- mpu_6050_acclX/mpu_6050_acclX.ino
+++ mpu_6050_acclX/mpu_6050_acclX.ino
@@ -1,81 +1,60 @@
 #include <Wire.h>
 
-long accelX, accelY, accelZ;
-float gForceX, gForceY, gForceZ;
+long accelZ;            // variable to store raw accelerometer data
+float gForceZ;          // variable to store accelerometer data in g-forces
 
-long head = 0;
-
-float buffer[350];
+long long head = 0;     // variable to keep track of the array index
+float buffera[1800];    // array to store accelerometer data
 
 void setup() {
-  Serial.begin(2000000);
-  Wire.setClock(400000);
-  Wire.begin();
-  setupMPU();
-  uint16_t time = millis();
-  int a = 0;
-  while (head < 500) { //millis()-time < 1000
-    recordAccelRegisters();
-    //printData();
-    a++;
-  }
-
-  Serial.println(a);
-  for (long b = 0; a <= head; b++) {
-    Serial.println(buffer[b]);
-  }
+  Serial.begin(2000000);  // initialize serial communication
+  Wire.setClock(400000);  // set I2C clock speed
+  Wire.begin();           // initialize I2C communication
+  setupMPU();             // initialize MPU6050 sensor
 }
 
 void loop() {
+  if (Serial.available()) {   // check if there is serial data available
+    while (head < 1000) {     // record accelerometer data until 1000 samples are collected
+      recordAccelRegisters();  // call function to read accelerometer data
+    }
+
+    for (long b = 0; b != head; b++) {  // loop through the array and print each value
+      Serial.println(buffera[b], 8);    // print accelerometer data with 8 decimal places
+    }
+    head = 0;     // reset array index to 0
+
+    while (Serial.available()) {   // clear serial buffer
+      char inChar = (char)Serial.read();
+    }
+  }
 }
 
-void setupMPU() {
-  Wire.beginTransmission(0b1101000);  //This is the I2C address of the MPU (b1101000/b1101001 for AC0 low/high datasheet sec. 9.2)
-  Wire.write(0x6B);                   //Accessing the register 6B - Power Management (Sec. 4.28)
-  Wire.write(0b00000000);             //Setting SLEEP register to 0. (Required; see Note on p. 9)
-  Wire.endTransmission();
-  Wire.beginTransmission(0b1101000);  //I2C address of the MPU
-  Wire.write(0x1C);                   //Accessing the register 1C - Acccelerometer Configuration (Sec. 4.5)
-  Wire.write(0b00000000);             //Setting the accel to +/- 2g
-                                      // Wire.write(0b00011000); //Setting the accel to +/- 16g
-  Wire.endTransmission();
-  Wire.beginTransmission(0b1101000);  //I2C address of the MPU
-  Wire.write(0x1A);                   // CONFIG register
-  Wire.write(0x00);                   // set to zero (disables DLPF)
-  Wire.endTransmission();
+void setupMPU() {          // function to initialize MPU6050 sensor
+  Wire.beginTransmission(0b1101000);  // start I2C communication with MPU6050 sensor
+  Wire.write(0x6B);        // PWR_MGMT_1 register address
+  Wire.write(0b00000000);  // set to 0 to wake up the sensor and use internal clock
+  Wire.endTransmission();  // end I2C communication with MPU6050 sensor
+
+  Wire.beginTransmission(0b1101000);  // start I2C communication with MPU6050 sensor
+  Wire.write(0x1C);        // ACCEL_CONFIG register address
+  Wire.write(0b00000000);  // set to 0 to set accelerometer range to +/- 2g
+  Wire.endTransmission();  // end I2C communication with MPU6050 sensor
+
+  Wire.beginTransmission(0b1101000);  // start I2C communication with MPU6050 sensor
+  Wire.write(0x1A);        // CONFIG register address
+  Wire.write(0x00);        // set to 0 to disable digital low-pass filter
+  Wire.endTransmission();  // end I2C communication with MPU6050 sensor
 }
 
-void recordAccelRegisters() {
-  Wire.beginTransmission(0b1101000);  //I2C address of the MPU
-  Wire.write(0x3f);                   //Starting register for Accel Readings
-  Wire.endTransmission();
-  Wire.requestFrom(0b1101000, 2);  //Request Accel Registers (3B - 40)
-  while (Wire.available() < 2)
-    ;
-  //accelX = Wire.read() << 8 | Wire.read();  //Store first two bytes into accelX
-  //accelY = Wire.read() << 8 | Wire.read();  //Store middle two bytes into accelY
-  accelZ = Wire.read() << 8 | Wire.read();  //Store last two bytes into accelZ
-  //gForceX = accelX / 16384.0;               //lowest detect range 2 g
-  //gForceY = accelY / 16384.0;
-  gForceZ = accelZ / 16384.0;
-
-  // gForceX = accelX / 2048.0;
-  // gForceY = accelY / 2048.0;
-  // gForceZ = accelZ / 2048.0;
-
-  buffer[head] = gForceZ;
-  head++;
-}
-
-void printData() {
-  Serial.print("0 ");
-  Serial.println(gForceZ, 8);
-
-
-
-  //Serial.print(atan(gForceX/gForceZ));
-  //Serial.print(" ");
-  //Serial.println(atan(gForceY));
-  //  Serial.print(" ");
-  //  Serial.println(gForceZ);
+void recordAccelRegisters() {   // function to read accelerometer data
+  Wire.beginTransmission(0b1101000);  // start I2C communication with MPU6050 sensor
+  Wire.write(0x3f);        // register address for Z-axis accelerometer data
+  Wire.endTransmission();  // end I2C communication with MPU6050 sensor
+  Wire.requestFrom(0b1101000, 2);     // request 2 bytes of data from MPU6050 sensor
+  while (Wire.available() < 2);      // wait for the data to be available
+  accelZ = Wire.read() << 8 | Wire.read();  // combine the two bytes into a single value
+  gForceZ = accelZ / 16384.0;    // convert raw accelerometer data to g-forces
+  buffera[head] = gForceZ;      // store g-force data in the array
+  head++;                       // increment array index
 }
