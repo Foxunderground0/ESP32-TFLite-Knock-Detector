bf7d67afd3e37c7824729d20fa01e827cba98a22
Foxunderground0 <umerirfan1205@gmail.com>
2023-06-19 12:23:08 +0500

    Quantization Error

--- ESP32/main/ESP32.cpp
+++ ESP32/main/ESP32.cpp
@@ -38,6 +38,7 @@ constexpr int kTensorArenaSize = 150000; // Define the size of the tensor arena 
 // Create a buffer for the interpreter tensor arena
 uint8_t EXT_RAM_BSS_ATTR tensor_arena[kTensorArenaSize];
 
+tflite::MicroInterpreter* interpreter = nullptr;
 
 // Interrupt service routine (ISR)
 bool IRAM_ATTR timer_isr(gptimer_t* gptimer, const gptimer_alarm_event_data_t* event_data, void* arg) {
@@ -48,48 +49,50 @@ bool IRAM_ATTR timer_isr(gptimer_t* gptimer, const gptimer_alarm_event_data_t* e
 }
 
 extern "C" void app_main() {
-    i2c_master_init();
+    /*
+        i2c_master_init();
 
-    MPU_write(0x6b, 0x00);
+        MPU_write(0x6b, 0x00); //Set Power Registers
 
-    gpio_pad_select_gpio(ONBOARD_LED);
-    gpio_set_direction(ONBOARD_LED, GPIO_MODE_OUTPUT);
-    gpio_set_level(ONBOARD_LED, led_state);
-    led_state = !led_state;
+        gpio_pad_select_gpio(ONBOARD_LED);
+        gpio_set_direction(ONBOARD_LED, GPIO_MODE_OUTPUT);
+        gpio_set_level(ONBOARD_LED, led_state);
+        led_state = !led_state;
 
-    ESP_LOGI(TAG, "Create timer handle");
-    gptimer_handle_t gptimer = NULL;
+        ESP_LOGI(TAG, "Create timer handle");
+        gptimer_handle_t gptimer = NULL;
 
-    gptimer_config_t timer_config = {
-    .clk_src = GPTIMER_CLK_SRC_DEFAULT,
-    .direction = GPTIMER_COUNT_UP,
-    .resolution_hz = 20000000, // 10MHz, 1 tick=1us
-    .flags = {0}
-    };
+        gptimer_config_t timer_config = {
+        .clk_src = GPTIMER_CLK_SRC_DEFAULT,
+        .direction = GPTIMER_COUNT_UP,
+        .resolution_hz = 20000000, // 10MHz, 1 tick=1us
+        .flags = {0}
+        };
 
-    ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer));
+        ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer));
 
-    gptimer_event_callbacks_t cbs = {
-        .on_alarm = timer_isr,
-    };
+        gptimer_event_callbacks_t cbs = {
+            .on_alarm = timer_isr,
+        };
 
-    ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &cbs, NULL));
-    ESP_LOGI(TAG, "Enable timer");
-    ESP_ERROR_CHECK(gptimer_enable(gptimer));
-    ESP_LOGI(TAG, "Start timer, stop it at alarm event");
+        ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer, &cbs, NULL));
+        ESP_LOGI(TAG, "Enable timer");
+        ESP_ERROR_CHECK(gptimer_enable(gptimer));
+        ESP_LOGI(TAG, "Start timer, stop it at alarm event");
 
-    gptimer_alarm_config_t alarm_config1 = {
-        .alarm_count = 11764,// 1700 times a second
-        .reload_count = 0,
-        .flags = {
-            .auto_reload_on_alarm = true
-        }
-    };
+        gptimer_alarm_config_t alarm_config1 = {
+            .alarm_count = 11764,// 1700 times a second
+            .reload_count = 0,
+            .flags = {
+                .auto_reload_on_alarm = true
+            }
+        };
+    */
 
     // Set up the model
     const tflite::Model* model = tflite::GetModel(model_data);
     tflite::MicroErrorReporter micro_error_reporter;
-    constexpr int kOpResolverSize = 6; // Define the number of supported operations
+    constexpr int kOpResolverSize = 7; // Define the number of supported operations
     tflite::MicroMutableOpResolver<kOpResolverSize> micro_op_resolver;
 
     micro_op_resolver.AddAveragePool2D();
@@ -98,30 +101,56 @@ extern "C" void app_main() {
     micro_op_resolver.AddRelu();
     micro_op_resolver.AddReshape(); //  https://stackoverflow.com/questions/62580548/is-the-keras-function-flatten-supported-by-tensorflow-lite
     micro_op_resolver.AddSoftmax();
+    micro_op_resolver.AddLogistic();
 
     //micro_op_resolver.AddBuiltin(tflite::BuiltinOperator_DEPTHWISE_CONV_2D, tflite::ops::micro::Register_DEPTHWISE_CONV_2D());
 
-    tflite::MicroInterpreter interpreter(model, micro_op_resolver, tensor_arena, kTensorArenaSize);
+    tflite::MicroInterpreter static_interpreter(model, micro_op_resolver, tensor_arena, kTensorArenaSize);
+    interpreter = &static_interpreter;
 
-    ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarm_config1));
-    ESP_ERROR_CHECK(gptimer_start(gptimer));
+    // Allocate memory from the tensor_arena for the model's tensors.
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk) {
+        printf("ateTensors() failed");
+        return;
+    }
+    printf("Success");
+    /*
+    int inputTensorIndex = 0;  // Adjust the index based on your model
+    int outputTensorIndex = 0;  // Adjust the index based on your model
 
-
-    uint64_t microseconds1 = esp_timer_get_time();
-
-    while (buffer_head < 1700) {
-        if (sample_ready) {
-            uint8_t* data_pointer = MPU_read(0x3f, 2);
-            buffer[buffer_head] = (float)((uint16_t)(data_pointer[0] << 8) | (data_pointer[1]));
-            buffer_head++;
-            //printf("Timer triggred");
-            free(data_pointer);
-            sample_ready = false;
-        }
+    const TfLiteTensor* inputTensor = interpreter->input_tensor(inputTensorIndex);
+    if (inputTensor->type != kTfLiteUInt8) {
+        // Handle error: Input tensor is not quantized to 8 bits
     }
 
-    uint64_t microseconds2 = esp_timer_get_time();
-    uint64_t diff = microseconds2 - microseconds1;
+    const float inputScale = inputTensor->params.scale;
+    const int inputZeroPoint = inputTensor->params.zero_point;
 
-    printf("Time in microseconds: %lld, head at: %d\n", diff, buffer_head);
+    printf("Input Scale: %f, Zero Point: %d\n", inputScale, inputZeroPoint);
+    */
+
+    /*
+        ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer, &alarm_config1));
+        ESP_ERROR_CHECK(gptimer_start(gptimer));
+
+
+        uint64_t microseconds1 = esp_timer_get_time();
+
+        while (buffer_head < 1700) {
+            if (sample_ready) {
+                uint8_t* data_pointer = MPU_read(0x3f, 2);
+                buffer[buffer_head] = (float)((uint16_t)(data_pointer[0] << 8) | (data_pointer[1]));
+                buffer_head++;
+                //printf("Timer triggred");
+                free(data_pointer);
+                sample_ready = false;
+            }
+        }
+
+        uint64_t microseconds2 = esp_timer_get_time();
+        uint64_t diff = microseconds2 - microseconds1;
+
+        printf("Time in microseconds: %lld, head at: %d\n", diff, buffer_head);
+    */
 };
