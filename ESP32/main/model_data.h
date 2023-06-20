77c5d2da8102575909c83b80a65395f720c67f8b
Foxunderground0 <umerirfan1205@gmail.com>
2023-06-20 17:05:56 +0500

    Model Works

--- ESP32/main/model_data.h
+++ ESP32/main/model_data.h
@@ -1,7 +1,7 @@
 #ifndef MODEL_H
 #define MODEL_H
 
-extern const unsigned char model_data[];
-extern const unsigned int model_data_len;
+extern const char model_data[];
+extern const long model_data_len;
 
 #endif  // MODEL_H
