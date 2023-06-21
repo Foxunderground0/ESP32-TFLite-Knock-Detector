1760a4bfe9700f15c2bddebfe179a7ab7a85adab
Foxunderground0 <umerirfan1205@gmail.com>
2023-06-22 00:58:17 +0500

    Added Heap Monitor

--- ESP32/main/ESP32.cpp
+++ ESP32/main/ESP32.cpp
@@ -16,74 +16,80 @@
 #include "model_data.h"
 #include "i2c_functions.h"
 
-#define ONBOARD_LED  GPIO_NUM_0
+#define ONBOARD_LED GPIO_NUM_0
 
-bool led_state = false;
-bool sample_ready = false;
+int led_state = 0;
 
-float input_data[1][1000][1][1];
+uint8_t input_data[1][1000][1][1];
 
-#define SAMPLES_COUNT 2000
-float buffer[SAMPLES_COUNT]; //Circular Buffer
+#define SAMPLES_COUNT 1700
+uint8_t buffer[SAMPLES_COUNT]; // Circular Buffer
 uint16_t buffer_head = 0;
 
-constexpr int kTensorArenaSize = 150000; // Define the size of the tensor arena buffer
+constexpr int kTensorArenaSize = 15000; // Define the size of the tensor arena buffer
 uint8_t EXT_RAM_BSS_ATTR tensor_arena[kTensorArenaSize];
 tflite::MicroInterpreter* interpreter = nullptr;
 
+#include <esp_heap_caps.h>
 
-IRAM_ATTR void data_collector(void* parameter) {
+/*
+// Function to print the current heap memory usage
+void printHeapUsage() {
+	size_t freeHeap = esp_heap_caps_get_free_size(MALLOC_CAP_8BIT);
+	printf("Free heap: %u bytes\n", freeHeap);
+}
+*/
+
+void data_collector(void* parameter) {
 	while (1) {
 		buffer_head = 0;
-		uint64_t microseconds1 = esp_timer_get_time();
+
+		// uint64_t microseconds1 = esp_timer_get_time();
+
 		uint64_t last_sample = esp_timer_get_time();
 		while (buffer_head < 1700) {
 			if (esp_timer_get_time() - last_sample > 588) {
 				last_sample = esp_timer_get_time();
 				uint8_t* data_pointer = MPU_read(0x3f, 2);
-				buffer[buffer_head] = ((float)((uint16_t)(data_pointer[0] << 8) | (data_pointer[1])) / 32768.0);
+				buffer[buffer_head] = (uint8_t)(((float)((uint16_t)(data_pointer[0] << 8) | (data_pointer[1])) / 32768.0) / 0.00392157);
 				buffer_head++;
 
-				//printf("Timer triggred");
+				// printf("Timer triggered");
 				free(data_pointer);
-				sample_ready = false;
 			}
 		}
 
-		uint64_t microseconds2 = esp_timer_get_time();
-		uint64_t diff = microseconds2 - microseconds1;
-
-		//printf("Time in microseconds: %lld, head at: %d\n", diff, buffer_head);
+		// uint64_t microseconds2 = esp_timer_get_time();
+		// uint64_t diff = microseconds2 - microseconds1;
+		// printf("Time in microseconds: %lld, head at: %d\n", diff, buffer_head);
 
 		// Reset the Watchdog Timer
 		vTaskDelay(1);
 	}
-
 }
 
 extern "C" IRAM_ATTR void app_main() {
 	i2c_master_init();
-	MPU_write(0x6b, 0x00); //Set Power Registers
+	MPU_write(0x6b, 0x00); // Set Power Registers
 
 	gpio_pad_select_gpio(ONBOARD_LED);
 	gpio_set_direction(ONBOARD_LED, GPIO_MODE_OUTPUT);
 	gpio_set_level(ONBOARD_LED, led_state);
 
-	led_state = !led_state;
-
 	// Set up the model
 	const tflite::Model* model = tflite::GetModel(model_data);
 	tflite::MicroErrorReporter micro_error_reporter;
-	constexpr int kOpResolverSize = 7; // Define the number of supported operations
+	constexpr int kOpResolverSize = 8; // Define the number of supported operations
 	tflite::MicroMutableOpResolver<kOpResolverSize> micro_op_resolver;
 
 	micro_op_resolver.AddAveragePool2D();
 	micro_op_resolver.AddConv2D();
 	micro_op_resolver.AddFullyConnected();
 	micro_op_resolver.AddRelu();
-	micro_op_resolver.AddReshape(); //  https://stackoverflow.com/questions/62580548/is-the-keras-function-flatten-supported-by-tensorflow-lite
+	micro_op_resolver.AddReshape(); // https://stackoverflow.com/questions/62580548/is-the-keras-function-flatten-supported-by-tensorflow-lite
 	micro_op_resolver.AddSoftmax();
 	micro_op_resolver.AddLogistic();
+	micro_op_resolver.AddQuantize();
 
 	tflite::MicroInterpreter static_interpreter(model, micro_op_resolver, tensor_arena, kTensorArenaSize);
 	interpreter = &static_interpreter;
@@ -106,13 +112,15 @@ extern "C" IRAM_ATTR void app_main() {
 		uint16_t tail = buffer_head;
 
 		for (int i = 1000; i >= 0; i--) {
-			input_data[0][i][0][0] = buffer[tail];
+			input_data[0][i][0][0] = (buffer[tail]);
 			tail--;
 			if (tail > 1800) {
 				tail = 1700;
 			}
-			//printf("%d ", tail);
 		}
+
+		// printf("%d ", tail);
+
 		memcpy(input_tensor->data.f, input_data, sizeof(input_data));
 
 		TfLiteStatus invoke_status = interpreter->Invoke();
@@ -121,9 +129,8 @@ extern "C" IRAM_ATTR void app_main() {
 		}
 
 		// Access the output tensor data
-		float* output_data = output_tensor->data.f;
+		uint8_t* output_data = output_tensor->data.uint8;
 
-		// Print the output values
 		// Get the size of the output tensor
 		int output_size = 1;
 		for (int i = 0; i < output_tensor->dims->size; i++) {
@@ -133,20 +140,25 @@ extern "C" IRAM_ATTR void app_main() {
 		// Print the output values
 		printf("Output: ");
 		for (int i = 0; i < output_size; i++) {
-			printf("%f ", output_data[i]);
+			printf("%f ", (float)output_data[i] * 0.00390625);
 		}
 
-		if (output_data[0] > 0.85) {
+		if (((float)output_data[0] * 0.00390625) > 0.85) {
 			gpio_set_level(ONBOARD_LED, 1);
 		} else {
 			gpio_set_level(ONBOARD_LED, 0);
 		}
 
+		//printf("  %d  ", led_state);
+
 		uint64_t microseconds2 = esp_timer_get_time();
 		uint64_t diff = microseconds2 - microseconds1;
 
 		printf("Time in microseconds: %lld \n", diff);
-		vTaskDelay(1);
 
+		//printHeapUsage();
+
+		vTaskDelay(1);
 	}
-};
+
+}
